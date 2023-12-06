39d0ee6e2424b327f962f4383ed349a5e3cfb683
Rich Evans <richard.evans@asos.com>
2023-12-06 13:52:11 +0000

    first commit

---
+++ src/Part1/Program.cs
@@ -0,0 +1,36 @@
+﻿var input = File.ReadAllLines("./input.txt");
+//input[0] = "Time:      7  15   30";
+//input[1] = "Distance:  9  40  200";
+var timeLine = input[0].Split(':');
+var times = timeLine[1].Trim().Split(" ").Where(x => !string.IsNullOrEmpty(x)).ToArray(); ;
+
+var distanceLine = input[1].Split(':');
+var distances =  distanceLine[1].Trim().Split(" ").Where(x => !string.IsNullOrEmpty(x)).ToArray(); ;
+
+var races = new List<(int, int)>();
+for (int i = 0; i < times.Length; i++)
+{
+    races.Add((int.Parse(times[i]), int.Parse(distances[i])));
+}
+
+var marginOfErrors = new List<int>();
+foreach (var race in races)
+{
+    var lowestTimeHeld = int.MinValue;
+    for (int i = 0; i <= race.Item1/2; i++)
+    {
+        if (GetDistance(i, race.Item1 - i) > race.Item2)
+        {
+            lowestTimeHeld = i;
+            break;
+        }
+    }
+    marginOfErrors.Add(race.Item1 - lowestTimeHeld + 1 - lowestTimeHeld);
+}
+
+int GetDistance(int buttonHeld, int timeRemaining)
+{
+    return buttonHeld * timeRemaining;
+}
+
+Console.WriteLine(marginOfErrors.Aggregate(1, (acc, val) => acc * val));
---
+++ src/Part2/Program.cs
@@ -0,0 +1,20 @@
+﻿var input = File.ReadAllLines("./input.txt");
+var timeLine = input[0].Split(':');
+var time = int.Parse(timeLine[1].Trim().Replace(" ", string.Empty)); ;
+var distanceLine = input[1].Split(':');
+long distance = long.Parse(distanceLine[1].Trim().Replace(" ", string.Empty));
+var lowestTimeHeld = long.MinValue;
+for (long i = 0; i <= time / 2; i++)
+{
+    if (GetDistance(i,time - i) > distance)
+    {
+        lowestTimeHeld = i;
+        break;
+    }
+}
+Console.WriteLine(time - lowestTimeHeld + 1 - lowestTimeHeld);
+
+long GetDistance(long buttonHeld, long timeRemaining)
+{
+    return buttonHeld * timeRemaining;
+}
